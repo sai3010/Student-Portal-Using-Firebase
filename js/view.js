0cc2e7a4d5e123bbbda4b136ae4375333661c37c
sai3010 <saiprasanth2007@gmail.com>
2018-01-15 13:59:27 +0530

    done i guess

--- js/view.js
+++ js/view.js
@@ -15,10 +15,14 @@
     databaseRef.once('value', function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
                 var childData = childSnapshot.val();  //gets all the child data
-                //var row=document.writeln("<div>");
-                //row.innerHTML=""+childData.title+"";
-                //row.className="container row col s12 m6 card blue-grey darken-1 card-content white-text"
-                title.innerHTML=""+childData.title+"";
-                msg.innerHTML=""+childData.message+"";
+                cont=$('<div/>').attr("class","container").appendTo(document.body);
+                row=$('<div/>').attr("class","row").appendTo(cont);
+                col=$('<div/>').attr("class","col s12 m12 l12").appendTo(row);
+                cardcolor=$('<div/>').attr("class","card blue-grey darken-1").appendTo(col);
+                card=$('<div/>').attr("class","card-content white-text").appendTo(cardcolor);
+                cardcont = $('<span/>').attr("class", "card-title").text(childData.title).appendTo(card);
+                para=$('<p/>').text(childData.message).appendTo(cardcont);
+                card=$('<div/>').attr("class","card-action").appendTo(para);
+                a1=$('<a/>').attr("class","date").text(childData.date).appendTo(card);
             });
         });
